43c854211b982e57317975d9858908baffa64d6c
Cedric GESTES <cedric.gestes@gmail.com>
2019-09-11 19:48:28 +0200

    WIP: add a mention button

--- packages/zefyr/lib/src/widgets/field.dart
+++ packages/zefyr/lib/src/widgets/field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zefyr/zefyr.dart';
 
 import 'controller.dart';
 import 'editor.dart';
@@ -19,6 +20,7 @@ class ZefyrField extends StatefulWidget {
   final ZefyrMode mode;
   final ZefyrToolbarDelegate toolbarDelegate;
   final ZefyrImageDelegate imageDelegate;
+  final ZefyrSearchDelegate searchDelegate;
   final ScrollPhysics physics;
 
   const ZefyrField({
@@ -31,6 +33,7 @@ class ZefyrField extends StatefulWidget {
     this.mode,
     this.toolbarDelegate,
     this.imageDelegate,
+    this.searchDelegate,
     this.physics,
   }) : super(key: key);
 
@@ -49,6 +52,7 @@ class _ZefyrFieldState extends State<ZefyrField> {
       mode: widget.mode ?? ZefyrMode.edit,
       toolbarDelegate: widget.toolbarDelegate,
       imageDelegate: widget.imageDelegate,
+      searchDelegate: widget.searchDelegate,
       physics: widget.physics,
     );
 
